ab7ebd3a2f12c1e77633bad08bddf95d4fbc7839
Roy Reneker <89213640+p2reneker25@users.noreply.github.com>
2021-11-18 17:35:46 +0000

    Stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var finalData = []
 function update()
 {
   //API returning object when fetched.
@@ -6,8 +6,8 @@ const time = Date.now();
 var lat = document.getElementById("lat").value
 var lng = document.getElementById("lgn").value
 var src = "sg"
-var stuffToGet=['swellHeight', 'waveHeight', 'windSpeed'];
-
+var stuffToGet=['windSpeed', 'waveHeight', 'swellHeight'];
+document.getElementById("image").src = "images/wait.png"
 
 fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${stuffToGet}&source=${src}&`, {
   headers: {
@@ -16,31 +16,47 @@ fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=$
   }
 }).then((response) => response.json()).then((jsonData) => {
     var data = JSON.stringify(jsonData)
-    console.log(data)
+    //console.log(data)
+    //console.log(data)
+    filter(data)
 
 });
 
 }
 
-function filter()
+function filter(data)
 {
-  var data = update()
+  //alert(data)
   var data2 = data.split(",")
   var counter = 2
   while(counter<5)
   {
-  console.log(data2 [counter]);
+ // console.log(data2[counter].toString().split(":").pop().replace("}", "").replace("}", ""));
+  finalData.push(data2[counter].toString().split(":").pop().replace("}", "").replace("}", "")); // cleanup array for only ints and place them in new array
   counter++
+  
   }
+  //alert(finalData)
+  decision(finalData[0], finalData[1], finalData[2])//call prediction function with data
 }
 
-function dive(swellHeight, waveHeight, windSpeed)
+function decision(waveHeight, windSpeed, swellHeight)
 {
-  var thresholds = [5,5,15]
-  var counter=0
-  while(counter<2)
+  var thresholds = [1.3,4,1.3]
+  //alert(swellHeight)
+  //alert(waveHeight)
+  //alert(windSpeed)
+  if(waveHeight<thresholds[0] && windSpeed<thresholds[1] && swellHeight<thresholds[2])
   {
-    counter++    
+    //alert("Good to dive")
+    document.getElementById("image").src = "images/go.png"
   }
-
+  else{
+    //alert("No sir!")
+    document.getElementById("image").src = "images/stop.png"
+  }
+  //output values to text
+  document.getElementById("data").innerHTML = "Wave Height: "+waveHeight+"m, Wind Speed: " +windSpeed+"m/s, Swell Height: "  + swellHeight+"m"
 }
+
+//wave wind swell
