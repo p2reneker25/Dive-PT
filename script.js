a4791368f94f87d069dfd9046c76c0818099a626
Roy Reneker <89213640+p2reneker25@users.noreply.github.com>
2021-11-19 17:30:29 +0000

    now it gets angry at you!

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function update()
 {
   //API returning object when fetched.
 const time = Date.now();
+document.getElementById("data").innerHTML = "Loading"
 var lat = document.getElementById("lat").value
 var lng = document.getElementById("lgn").value
 var src = "sg"
 var stuffToGet=['windSpeed', 'waveHeight', 'swellHeight'];
 document.getElementById("image").src = "images/wait.png"
+//Check for blank data
+if(/\p{L}/u.test(lng) || /\p{L}/u.test(lat) || lng == "" || lat=="")
+{
+  document.getElementById("data").innerHTML = "ERROR: Location error. Check lat/lng and try again!"
+  document.getElementById("image").src = "images/warn.png"
+}
 
 fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${stuffToGet}&source=${src}&`, {
   headers: {
@@ -16,8 +23,6 @@ fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=$
   }
 }).then((response) => response.json()).then((jsonData) => {
     var data = JSON.stringify(jsonData)
-    //console.log(data)
-    //console.log(data)
     filter(data)
 
 });
@@ -26,37 +31,34 @@ fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=$
 
 function filter(data)
 {
-  //alert(data)
   var data2 = data.split(",")
   var counter = 2
   while(counter<5)
   {
- // console.log(data2[counter].toString().split(":").pop().replace("}", "").replace("}", ""));
+  console.log(data2[counter].toString().split(":").pop().replace("}", "").replace("}", ""));
   finalData.push(data2[counter].toString().split(":").pop().replace("}", "").replace("}", "")); // cleanup array for only ints and place them in new array
   counter++
   
   }
-  //alert(finalData)
   decision(finalData[0], finalData[1], finalData[2])//call prediction function with data
 }
 
 function decision(waveHeight, windSpeed, swellHeight)
 {
   var thresholds = [1.3,4,1.3]
-  //alert(swellHeight)
-  //alert(waveHeight)
-  //alert(windSpeed)
+
   if(waveHeight<thresholds[0] && windSpeed<thresholds[1] && swellHeight<thresholds[2])
   {
-    //alert("Good to dive")
     document.getElementById("image").src = "images/go.png"
   }
   else{
-    //alert("No sir!")
     document.getElementById("image").src = "images/stop.png"
   }
   //output values to text
   document.getElementById("data").innerHTML = "Wave Height: "+waveHeight+"m, Wind Speed: " +windSpeed+"m/s, Swell Height: "  + swellHeight+"m"
 }
-
-//wave wind swell
+function date()
+{
+    var date = Date.now()
+    alert(date)
+}
