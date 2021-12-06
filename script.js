a94c596fde14876257a981e64bef5838d4a452d1
Roy Reneker <89213640+p2reneker25@users.noreply.github.com>
2021-12-06 17:21:53 +0000

    Done?

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function update() {
 			return
 		}
 		
-		decision(wave, wind, swell)
+		decision(wave, wind, swell, "user")
 	}
 }
 
@@ -66,19 +66,22 @@ function filter(data) {
 		counter++
 
 	}
-	decision(finalData[0], finalData[1], finalData[2]) //call prediction function with data
+	decision(finalData[0], finalData[1], finalData[2], "api") //call prediction function with data
 }
 
-function decision(waveHeight, windSpeed, swellHeight) {
+function decision(waveHeight, windSpeed, swellHeight, source) {
 	var thresholds = [1.3, 4, 1.3]
-
 	if (waveHeight < thresholds[0] && windSpeed < thresholds[1] && swellHeight < thresholds[2]) {
 		document.getElementById("image").src = "images/go.png"
 	} else {
 		document.getElementById("image").src = "images/stop.png"
 	}
-	//output values to text
-	document.getElementById("data").innerHTML = "Wave Height: " + waveHeight + "m, Wind Speed: " + windSpeed + "m/s, Swell Height: " + swellHeight + "m"
+  if(source == "api"){
+    document.getElementById("data").innerHTML = "Wave Height: " + waveHeight + "m, Wind Speed: " + windSpeed + "m/s, Swell Height: " + swellHeight + "m"
+  } else{
+    document.getElementById("data").innerHTML =  ""
+  }
+	
 }
 
 function date() {
